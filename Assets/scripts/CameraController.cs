20a74a799a58eeb89b7e26022d17dce7e1bb5204
joe yarnall <joe.yarnall@gmail.com>
2015-05-11 15:54:20 +0100

    Limit on min and max zoom

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -1,28 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
-
+public class CameraController : MonoBehaviour
+{
     public float ScrollSpeed = 10f;
     public float ZoomSpeed = 100f;
-    
-	void FixedUpdate () {
+
+    public float MaxZoom = 8f;
+    public float MinZoom = 2f;
+
+    private void FixedUpdate()
+    {
         Move();
-	}
+    }
 
     private void Move()
     {
-        var h = Input.GetAxis("Horizontal");
-        var v = Input.GetAxis("Vertical");
-        var zoom = -Input.GetAxis("Mouse ScrollWheel");
+        var h = Input.GetAxis("Horizontal") * ScrollSpeed;
+        var v = Input.GetAxis("Vertical") * ScrollSpeed;
+        var zoom = -Input.GetAxis("Mouse ScrollWheel") * ZoomSpeed;
 
         var component = GetComponent<Rigidbody>();
 
-        var velocity = new Vector3(
-            ScrollSpeed * h,
-            zoom * ZoomSpeed,
-            ScrollSpeed * v);
+        var velocity = new Vector3(h, zoom, v);
 
-        component.velocity = velocity;	
+        if ((component.position.y > MaxZoom && zoom > 0) || (component.position.y < MinZoom && zoom < 0))
+        {
+            velocity.y = 0;
+        }
+
+        component.velocity = velocity;
     }
 }
